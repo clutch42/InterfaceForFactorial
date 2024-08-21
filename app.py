44633fbfb270d6105675a162dd20f2a2a6c27f09
Brian Engel <clutch_42@yahoo.com>
2024-08-21 12:14:34 -0500

    added test server

--- app.py
+++ app.py
@@ -8,5 +8,12 @@ def index():
     return "Hello, World!"
 
 
+# Define additional routes here
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    # Example of a route that returns JSON data
+    return jsonify({"message": "Hello from the API!"})
+
+
 if __name__ == '__main__':
     app.run(debug=True)
